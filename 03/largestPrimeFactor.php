92f4fe6d1f4cb522c62d2062d33322e7a29a37c6
eddst <edmond.daoust@gmail.com>
2018-03-29 20:59:48 +0200

    Solved 03/largestPrimeFactor

--- 03/largestPrimeFactor.php
+++ 03/largestPrimeFactor.php
@@ -1,5 +1,29 @@
 <?php
+/*
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+ */
 
-function largestPrimeFactor(){
-
+function checkPrime($n){
+    for ($x = 2; $x <= $n/2; $x++) {
+        if ($n % $x == 0){
+            return false;
+        }
+    }
+    return true;
 }
+
+function largestPrimeFactor($num){
+    for($i = 1; $i <= $num; $i++){
+        if($num % $i == 0){
+            $prime = $num / $i;
+            if(checkPrime($prime)){
+                echo $prime;
+                break;
+            }
+        }
+    }
+}
+
+
+largestPrimeFactor(600851475143);
